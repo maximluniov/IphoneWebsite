fafa35d7e7db47ca7d7b3c2fe914a6be609c4209
maximluniov <mlunev330@gmail.com>
2024-07-01 20:05:17 +0200

    error fix

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -27,7 +27,7 @@ const Highlights = () => {
             <p className='link'>Watch the film
               <img src={watchImg} alt="watch" className='ml-2'/>
             </p>
-            <p className='link'>Watch the film
+            <p className='link'>Watch the event
               <img src={rightImg} alt="right" className='ml-2'/>
             </p>
           </div>
